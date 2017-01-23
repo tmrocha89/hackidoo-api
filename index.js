1a0d5afd0ee0205a81a43928408adc3f39e02ff3
Tiago Rocha <tmrocha89@gmail.com>
2017-01-23 21:21:51 +0000

    port changed

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var jwt = require("jwt-simple");  
-var auth = require("./Auth.js")();  
-var users = require("./repository/users.js");  
-var cfg = require("./config.js"); 
+var jwt = require("jwt-simple");
+var auth = require("./Auth.js")();
+var users = require("./repository/users.js");
+var cfg = require("./config.js");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,14 +15,14 @@ app.use(bodyParser.json());
 
 app.use(auth.initialize());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('Vai a volta, porque daqui não passas');
 });
 
-app.post("/token", function(req, res) { 
+app.post("/token", function(req, res) {
     if (req.body.email && req.body.password) {
         var email = req.body.email;
         var password = req.body.password;
@@ -46,24 +46,25 @@ app.post("/token", function(req, res) {
 });
 
 app.get("/user", auth.authenticate(), function(req, res) {
-    res.json({works: true});
+    res.json({ works: true });
 });
 
-app.get('/collections', function(req, res){
+app.get('/collections', function(req, res) {
     res.send("Returning all collections");
 });
 
-app.get('/collection/:id/', function(req,res){
+app.get('/collection/:id/', function(req, res) {
     var id = req.params.id;
     res.send("Returning recipes from collection " + id);
 });
 
-app.post('/collection', function(req,res){
+app.post('/collection', function(req, res) {
     res.send("saving stuffs");
 });
 
-app.post('/collection/:id/recipe', function(req,res){
-    res.send("saving a recipe");
+app.post('/collection/:id/recipe', function(req, res) {
+    res.send(req.body);
+    //res.send("saving a recipe");
 });
 
 app.listen(port);
